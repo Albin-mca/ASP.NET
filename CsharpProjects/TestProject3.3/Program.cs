603ecbd7a8882beda3f27439744508b136e63e98
Albin George <81986569+godcy@users.noreply.github.com>
2024-03-01 20:57:09 +0400

    12th

---
+++ CsharpProjects/TestProject3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿string sku="01-MN-L ";
+string[] product=sku.Split('-');
+
+string type="";
+string color="";
+string size="";
+
+if(product[0]=="01")
+{
+    type="Sweet shirt";
+}else if(product[0]=="02")
+{
+    type="T-Shirt";
+}else if(product[0]=="03")
+{
+    type="Sweet Pants";
+}
+else{
+    type="Other";
+}
+
+if(product[1]=="BL")
+{
+    color="Black";
+}else if(product[1]=="MN")
+{
+    color="Maroon";
+}else
+{
+    color="White";
+}
+
+if(product[2]=="L")
+{
+    size="Large";
+}
+else
+{
+    size="one Sixze Fits all";
+}
+
+Console.WriteLine($"Product:{size} {color} {type}");
